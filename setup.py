35f70b2e6b9e62109d8fbefcf6205bb44236d4fc
vijaya657 <pawarvijaya666@gmail.com>
2024-01-01 22:03:13 +0530

    saved changes
    Signed-off-by: vijaya657 <pawarvijaya666@gmail.com>

--- setup.py
+++ setup.py
@@ -3,14 +3,12 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-
 __version__ = "0.0.0"
 
 REPO_NAME = "Kidney-Disease-Classification-Deep-Learning-Project"
 AUTHOR_USER_NAME = "vijaya657"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "pawarvijaya666@gmail.com"
-
 
 setuptools.setup(
     name=SRC_REPO,
@@ -19,7 +17,7 @@ setuptools.setup(
     author_email=AUTHOR_EMAIL,
     description="A small python package for CNN app",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",  # Corrected parameter name
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
